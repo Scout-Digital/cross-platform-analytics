c52cd0960699243ea7867eeb1c06b3312263969c
Trevor Brown <trevorbrown@Trevors-MacBook-Pro.local>
2023-11-13 07:44:25 -0700

    set up api calls for hubspot, add .env and .env-sample files for ease of key setup

--- cross-platform-analytics.py
+++ cross-platform-analytics.py
@@ -1,9 +1,10 @@
-import requests
+from hubspot import HubSpot
 import os
+import requests
 
 rest_base = "https://api.hubapi.com/crm/v3/objects/"
 
-client_api_token = os.environ.get('HUBSPOT_API_KEY')
+
 
 api_url = rest_base +  "?limit=100&archived=false"
 
@@ -12,3 +13,5 @@ api_url = "https://jsonplaceholder.typicode.com/todos/1"
 response = requests.get(api_url)
 
 print(response.json())
+
+
