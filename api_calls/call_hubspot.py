b94ee15df94e94cf19c9985394e51fc4b29058d1
Trevor Brown <trevorbrown@Trevors-MacBook-Pro.local>
2023-11-16 08:10:39 -0700

    update hubspot file with functioning api call

--- api_calls/call_hubspot.py
+++ api_calls/call_hubspot.py
@@ -1,43 +1,28 @@
 import os
-import hubspot
 from dotenv import load_dotenv
-import requests as req
-
-from pprint import pprint
-from hubspot.crm.imports import ApiException
-from hubspot import hubspot.files
-
-#Import necessary functions from HubSpot python package
-
-from hubspot import HubSpot
-# from hubspot import 
-
-from hubspot.auth.oauth import ApiException
-
-
-rest_base = 'https://api.hubapi.com'
-
-contacts_request_url = '/crm/v3/objects/contacts'
-
-
-# Get environment variables via dotenv package
+import requests
 
 load_dotenv()
 
-# API_USERNAME = os.getenv('CLIENT_HUBSPOT_EMAIL')
 API_KEY = os.getenv('CLIENT_HUBSPOT_TOKEN')
 
-print(API_KEY)
+import requests
 
-client = hubspot.Client.create(access_token=API_KEY)
+url = "https://api.hubapi.com/crm/v3/objects/contacts"
 
-print(client)
+payload = {}
+headers = {
+  'Authorization': 'Bearer ' + API_KEY
 
-try:
-    api_response = client.crm.imports.core_api.get_page()
-    pprint(api_response)
-except ApiException as e:
-    print("Exception when calling core_api->get_page: %s\n" % e)
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
+
+
+
 
 
 
--- cross-platform-analytics.py
+++ cross-platform-analytics.py
@@ -2,16 +2,5 @@ from hubspot import HubSpot
 import os
 import requests
 
-rest_base = "https://api.hubapi.com/crm/v3/objects/"
-
-
-
-api_url = rest_base +  "?limit=100&archived=false"
-
-api_url = "https://jsonplaceholder.typicode.com/todos/1"
-
-response = requests.get(api_url)
-
-print(response.json())
 
 
