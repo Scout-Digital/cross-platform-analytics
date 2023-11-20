f6e6103e60852039646f5d4bef4b69777e9282ea
Trevor Brown <trevorbrown@Trevors-MacBook-Pro.local>
2023-11-19 20:16:09 -0700

    add todos to complete hubspot function

--- api_calls/call_hubspot.py
+++ api_calls/call_hubspot.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+import json
 
 load_dotenv()
 
@@ -13,13 +14,43 @@ url = "https://api.hubapi.com/crm/v3/objects/contacts"
 payload = {}
 headers = {
   'Authorization': 'Bearer ' + API_KEY
-
 }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+# Set up global output vars
+all_contacts = list()
+count = 0
 
-print(response.text)
+## Function : get_all_contacts
+##  @params url
+##  @params contact
+##  **TODO**@params get_all - Boolean variable that triggers a pagination handler. Passing get_all will trigger a recursive component of the function designed to run until there is no logner a 'next' link in the pagination property of the response object
+##  @returns type dict() - All contacts in a Python Dictionary obejct
 
+def get_contacts(url, contacts_start):
+    
+    response = requests.request("GET", url, headers=headers, data=payload)
+    
+    result = response.json()
+    contacts = result['results']
+    
+    # Get data from pagination related data from result objects
+    p_terms = result['paging']
+
+    # merge two lists
+    contacts_updated = contacts_start + contacts
+    
+    return contacts_updated
+
+    ##TODO - Configure recursive function to handle pagination. This is currently causing a stack overflow error because there are ~25k pages of contacts
+    ##if p_terms['next']['link'] == '' : 
+    ##else :  
+      ## return get_contacts(p_terms['next']['link'], contacts_updated, count)
+
+    # Recall get contacts within get     
+  
+
+print(get_contacts(url, all_contacts))
+ 
 
 
 
